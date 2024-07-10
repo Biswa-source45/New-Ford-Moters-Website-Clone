57fad554d8449d0d7be68b00135606ecda6f4d74
Biswabhusan Sahoo <161637161+Biswa-source45@users.noreply.github.com>
2024-07-10 20:13:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function imgChange(){
+    var img1=document.querySelector("#imgcard1")
+    var img2=document.querySelector("#imgcard2")
+    var img3=document.querySelector("#imgcard3")
+    var img4=document.querySelector("#imgcard4")
+    img1.style.display="block"
+    img2.style.display="none"
+    img3.style.display="none"
+    img4.style.display="none"
+    
+
+}
+function imgchange2(){
+    var img1=document.querySelector("#imgcard1")
+    var img2=document.querySelector("#imgcard2")
+    var img3=document.querySelector("#imgcard3")
+    var img4=document.querySelector("#imgcard4")
+
+    img1.style.display="none"
+    img2.style.display="block"
+    img3.style.display="none"
+    img4.style.display="none"
+
+}
+function imgchange3(){
+    var img1=document.querySelector("#imgcard1")
+    var img2=document.querySelector("#imgcard2")
+    var img3=document.querySelector("#imgcard3")
+    var img4=document.querySelector("#imgcard4")
+    img1.style.display="none"
+    img2.style.display="none"
+    img3.style.display="block"
+    img4.style.display="none"
+
+}
+function imgchange4(){
+    var img1=document.querySelector("#imgcard1")
+    var img2=document.querySelector("#imgcard2")
+    var img3=document.querySelector("#imgcard3")
+    var img4=document.querySelector("#imgcard4")
+    img1.style.display="none"
+    img2.style.display="none"
+    img3.style.display="none"
+    img4.style.display="block"
+
+}
